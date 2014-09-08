2fcf66db6fff58cd7789ac51d5dad7a9e84b32f7
ccpjorundur <jorundur@ccpgames.com>
2014-09-08 10:41:10 +0000

    Adding two new columns

--- Tables/pilot.users.sql
+++ Tables/pilot.users.sql
@@ -2,7 +2,9 @@ CREATE TABLE [pilot].[users]
 (
 [userID] [int] NOT NULL IDENTITY(1, 1),
 [userName] [nvarchar] (200) COLLATE Latin1_General_CI_AI NOT NULL,
-[createDate] [datetime2] (0) NOT NULL CONSTRAINT [DF__users__createDat__5441852A] DEFAULT (getutcdate())
+[createDate] [datetime2] (0) NOT NULL CONSTRAINT [DF__users__createDat__5441852A] DEFAULT (getutcdate()),
+[new_column_1] [int] NULL,
+[new_column_2] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 ) ON [PRIMARY]
 GO
 ALTER TABLE [pilot].[users] ADD CONSTRAINT [users_PK] PRIMARY KEY CLUSTERED  ([userID]) ON [PRIMARY]
