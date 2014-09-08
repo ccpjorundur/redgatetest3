29f671798506e084cb6698ed0fee68ee25dd0bdc
ccpjorundur <jorundur@ccpgames.com>
2014-09-08 10:30:54 +0000

    Adding static data to sql source control

---
+++ Data/pilot.eventTypes_Data.sql
@@ -0,0 +1,4 @@
+INSERT INTO [pilot].[eventTypes] ([eventTypeID], [eventTypeName], [description]) VALUES (1, 'Logon', '')
+INSERT INTO [pilot].[eventTypes] ([eventTypeID], [eventTypeName], [description]) VALUES (2, 'Logoff', '')
+INSERT INTO [pilot].[eventTypes] ([eventTypeID], [eventTypeName], [description]) VALUES (3, 'VISK change', '')
+INSERT INTO [pilot].[eventTypes] ([eventTypeID], [eventTypeName], [description]) VALUES (4, 'XP change', '')
---
+++ Data/pilot.shipClasses_Data.sql
@@ -0,0 +1,3 @@
+INSERT INTO [pilot].[shipClasses] ([shipClassID], [shipClassName]) VALUES (1, N'Fighter')
+INSERT INTO [pilot].[shipClasses] ([shipClassID], [shipClassName]) VALUES (2, N'Heavy')
+INSERT INTO [pilot].[shipClasses] ([shipClassID], [shipClassName]) VALUES (3, N'Support')
