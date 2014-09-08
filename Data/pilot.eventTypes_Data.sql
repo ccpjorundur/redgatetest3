7432c03c90a1332639a45e422a41786a95146a0a
ccpjorundur <jorundur@ccpgames.com>
2014-09-08 10:32:55 +0000

    Changing static data

--- Data/pilot.eventTypes_Data.sql
+++ Data/pilot.eventTypes_Data.sql
@@ -1,4 +1,4 @@
 INSERT INTO [pilot].[eventTypes] ([eventTypeID], [eventTypeName], [description]) VALUES (1, 'Logon', '')
 INSERT INTO [pilot].[eventTypes] ([eventTypeID], [eventTypeName], [description]) VALUES (2, 'Logoff', '')
-INSERT INTO [pilot].[eventTypes] ([eventTypeID], [eventTypeName], [description]) VALUES (3, 'VISK change', '')
-INSERT INTO [pilot].[eventTypes] ([eventTypeID], [eventTypeName], [description]) VALUES (4, 'XP change', '')
+INSERT INTO [pilot].[eventTypes] ([eventTypeID], [eventTypeName], [description]) VALUES (3, 'VISK change', 'General VISK change event')
+INSERT INTO [pilot].[eventTypes] ([eventTypeID], [eventTypeName], [description]) VALUES (4, 'XP change', 'General XP change event')
